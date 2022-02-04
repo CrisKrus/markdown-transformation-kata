80414fc04f2cb88e0205e91710871fdceefdaa82
Cristian Suarez <cristian.suarez.ulpgct@gmail.com>
2022-02-04 15:35:58 +0000

    test: split anchor and url

--- test/test_url_finder.py
+++ test/test_url_finder.py
@@ -1,14 +1,31 @@
 import re
 
 INLINE_URL_PATTERN = re.compile(r'\[[^\[\]]+\]\([^\(\)]+\)')
+ANCHOR_TEXT_PATTERN = re.compile(r'\[[^\[\]]+\]')
+URL_PATTERN = re.compile(r'\([^\(\)]+\)')
 
 def url_finder(text: str) -> str:
     return INLINE_URL_PATTERN.findall(text)
 
-def test_finds_an_url():
+def split_inline_url(text: str) -> str:
+    """
+    Given a text with the format [some text](some-url.com)
+    returns both texts ('some text', 'some-url.com')
+    """
+    split_text = text.split("](")
 
+    anchor_text = split_text[0][1:]
+    url = split_text[1][:-1]
+
+    return anchor_text, url
+
+def test_finds_an_url():
     result = url_finder("[anchor](url.com)")
 
     assert result[0] == "[anchor](url.com)"
 
-    
+def test_split_anchor_and_url():
+    anchor, url = split_inline_url("[anchor](url.com)")
+
+    assert anchor == "anchor"
+    assert url == "url.com"
