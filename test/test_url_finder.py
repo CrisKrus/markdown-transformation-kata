eb5a9d9cf9d75c1072bd6e38c898648231fc8ef6
Cristian Suarez <cristian.suarez.ulpgct@gmail.com>
2022-02-04 15:52:23 +0000

    test: finds multiple urls

--- test/test_url_finder.py
+++ test/test_url_finder.py
@@ -6,6 +6,12 @@ def test_finds_an_url():
 
     assert result[0] == "[anchor](url.com)"
 
+def test_finds_multiple_urls():
+    result = URLFinder().find_all_inline_urls("[anchor](url.com) other stuff [new link](other-url.com)")
+
+    assert result[0] == "[anchor](url.com)"
+    assert result[1] == "[new link](other-url.com)"
+
 def test_split_anchor_and_url():
     anchor, url = URLFinder().split_inline_url("[anchor](url.com)")
 
